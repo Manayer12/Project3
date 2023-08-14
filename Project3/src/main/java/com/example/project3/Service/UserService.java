b5a3e82bf58ced30561711a357ab79141ed62d60
Manayer12 <102624614+Manayer12@users.noreply.github.com>
2023-08-14 17:15:32 +0300

    Update UserService.java

--- Project3/src/main/java/com/example/project3/Service/UserService.java
+++ Project3/src/main/java/com/example/project3/Service/UserService.java
@@ -1,12 +1,21 @@
 package com.example.project3.Service;
 
+import com.example.project3.Model.Product;
 import com.example.project3.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
 public class UserService {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private MerchantService merchantService;
+    @Autowired
+    private MarchentStockService marchentStockService;
+
     ArrayList<User>users=new ArrayList<>();
 
 
@@ -54,7 +63,7 @@ public class UserService {
     public boolean checkID(Integer id) {
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getId() == id ){
-             return true;}
+             return true;}///
 
 
         }
@@ -62,27 +71,54 @@ public class UserService {
     }
 
 
- public boolean checkbalance(int price) {
+ public void checkbalance(int price) {
      for (int i = 0; i < users.size(); i++) {
          if (users.get(i).getBalance() > price) {
              users.get(i).setBalance(users.get(i).getBalance() - price);
-             return true;
          }
-     }
- return  false;}
+     }}
 
-    public boolean addToBalance(int price) {
+
+
+    public void addToBalance(int price) {
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getBalance() > price) {
                 users.get(i).setBalance(users.get(i).getBalance() + price);
-                return true;
+
             }
+        }}
+
+
+    public boolean buyproduct(Integer pId,Integer mId,Integer userId){
+    boolean trueproduct=productService.checkPid(pId);
+    boolean truemerchant=merchantService.checkMid(mId);
+    boolean trueuser=checkID(userId);
+    boolean checkstock=marchentStockService.checkstock(pId);
+        if(truemerchant && trueproduct && trueuser){
+        if(checkstock){
+            checkbalance(productService.returnPrice(pId));
+           return true;
+
+                  }
         }
-        return  false;}
+
+        return false;}
+
+    public boolean returnProduct(Integer pId,Integer mId,Integer userId){
+        boolean trueproduct=productService.checkPid(pId);
+        boolean truemerchant=merchantService.checkMid(mId);
+        boolean trueuser=checkID(userId);
+        boolean checkstock=marchentStockService.checkstock(pId);
+        if(truemerchant && trueproduct && trueuser){
+            if(checkstock){
+                addToBalance(productService.returnPrice(pId));
+
+                return true;
+
+            } }
 
 
-
-
+        return false;}
 
 
 
