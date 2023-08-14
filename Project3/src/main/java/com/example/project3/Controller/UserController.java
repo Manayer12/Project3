1a0b433682a22e1be9d910b672d711b7ab242d97
Manayer12 <102624614+Manayer12@users.noreply.github.com>
2023-08-14 17:17:00 +0300

    Update UserController.java

--- Project3/src/main/java/com/example/project3/Controller/UserController.java
+++ Project3/src/main/java/com/example/project3/Controller/UserController.java
@@ -14,76 +14,64 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/user")
 public class UserController {
     private final UserService userService;
-    private final ProductService productService;
-    private final MerchantService merchantService;
-    private final MarchentStockService marchentStockService;
 
-@GetMapping("/get")
-    public ResponseEntity getAllUsers(){
+
+    @GetMapping("/get")
+    public ResponseEntity getAllUsers() {
 
         return ResponseEntity.status(200).body(userService.getAllUser());
-
     }
-@PostMapping("/add")
-    public ResponseEntity addUser(@RequestBody @Valid User user, Errors errors){
-        if (errors.hasErrors()){
-            String message=errors.getFieldError().getDefaultMessage();
+
+    @PostMapping("/add")
+    public ResponseEntity addUser(@RequestBody @Valid User user, Errors errors) {
+        if (errors.hasErrors()) {
+            String message = errors.getFieldError().getDefaultMessage();
             return ResponseEntity.status(400).body(message);
         }
-       userService.addUsers(user);
+        userService.addUsers(user);
         return ResponseEntity.status(200).body(new ApiResponse("user added"));
 
     }
 
-@PutMapping("/update/{id}")
-    public ResponseEntity updateUser(@PathVariable Integer id, @RequestBody @Valid User user,Errors errors){
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateUser(@PathVariable Integer id, @RequestBody @Valid User user, Errors errors) {
 
-        if (errors.hasErrors()){
-            String message=errors.getFieldError().getDefaultMessage();
+        if (errors.hasErrors()) {
+            String message = errors.getFieldError().getDefaultMessage();
             return ResponseEntity.status(400).body(message);
         }
-        boolean isUpdated=userService.updateUser(id,user);
+        boolean isUpdated = userService.updateUser(id, user);
         return ResponseEntity.status(200).body(new ApiResponse("user is updated"));
 
 
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity deleteUser(@PathVariable Integer id){
+    public ResponseEntity deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
         return ResponseEntity.status(200).body(new ApiResponse("user deleted"));
 
     }
 
+
     @PutMapping("buyproduct/{userId}/{pId}/{mId}")
-    public ResponseEntity buyProduct(@PathVariable Integer userId,@PathVariable Integer pId,@PathVariable Integer mId){
-        boolean trueproduct=productService.checkPid(pId);
-        boolean truemerchant=merchantService.checkMid(mId);
-        boolean trueuser=userService.checkID(userId);
-        boolean checkstock=marchentStockService.checkstock(pId);
-        if(truemerchant && trueproduct && trueuser){
-            if(checkstock){
-                userService.checkbalance(productService.returnPrice(pId));
-                return ResponseEntity.status(200).body(new ApiResponse("The product has been purchased"));
-            }}
+    public ResponseEntity buyProduct(@PathVariable Integer userId, @PathVariable Integer pId, @PathVariable Integer mId) {
+        boolean truepurchesed = userService.buyproduct(pId, mId, userId);
+        if (truepurchesed) {
+            return ResponseEntity.status(200).body(new ApiResponse("The product has been purchased"));
+            }
 
-              return ResponseEntity.status(400).body(new ApiResponse("The product can not be purchased"));
+        return ResponseEntity.status(400).body(new ApiResponse("The product can not be purchased"));
     }
-
 
 
     //return product to the merchant
     @PutMapping("returnProduct/{userId}/{pId}/{mId}")
-    public ResponseEntity returnProduct(@PathVariable Integer userId,@PathVariable Integer pId,@PathVariable Integer mId){
-        boolean trueproduct=productService.checkPid(pId);
-        boolean truemerchant=merchantService.checkMid(mId);
-        boolean trueuser=userService.checkID(userId);
-        boolean checkstock=marchentStockService.returnToStock(pId);
-        if(truemerchant && trueproduct && trueuser){
-            if(checkstock){
-                userService.addToBalance(productService.returnPrice(pId));
-                return ResponseEntity.status(200).body(new ApiResponse("The product has been returned"));
-            }}
+    public ResponseEntity returnProduct(@PathVariable Integer userId, @PathVariable Integer pId, @PathVariable Integer mId) {
+        boolean returnproduct = userService.returnProduct(pId, mId, userId);
+        if (returnproduct) {
+            return ResponseEntity.status(200).body(new ApiResponse("The product has been returned"));
+        }
 
         return ResponseEntity.status(400).body(new ApiResponse("The product can not be returned"));
     }
